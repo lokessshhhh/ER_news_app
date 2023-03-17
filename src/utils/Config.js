c47231cbe00fb4baa146e13f29a0d45b0fd15f49
Egor Zubkevich <zubkevich@msrc.org>
2023-03-17 13:54:47 -0400

    Added base url constant

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,5 +1,6 @@
-export const ApiBaseUrl = 'http://104.152.168.42/~healthca/staging-empire-reportny/wp-json/wp/v2/'
-export const Deeplink = 'https://empirereportapp.page.link/screens/'
-export const FCM_API_KEY='8p24q2315p_0s19q1000-6q3n77psn690ss674159rpqr-675n02s1ps953n277qor4s6s11q99'
-export const WP_JSON_URL = 'http://104.152.168.42/~healthca/staging-empire-reportny/wp-json/'
-export const POSTER_INITIAL_ROUTE = 'http://104.152.168.42/~healthca/staging-empire-reportny/?p='
+export const BASE_URL = 'http://104.152.168.42/~healthca/staging-empire-reportny';
+export const ApiBaseUrl = `${BASE_URL}/wp-json/wp/v2/`;
+export const Deeplink = 'https://empirereportapp.page.link/screens/';
+export const FCM_API_KEY = '8p24q2315p_0s19q1000-6q3n77psn690ss674159rpqr-675n02s1ps953n277qor4s6s11q99';
+export const WP_JSON_URL = `${BASE_URL}/wp-json/`;
+export const POSTER_INITIAL_ROUTE = `${BASE_URL}/?p=`;
